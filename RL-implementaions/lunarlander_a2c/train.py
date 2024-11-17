15b7d138c83f49d72626f31a2205285f513b9bd1
Vanh41 <vietanhm6a@gmail.com>
2024-11-17 16:41:48 +0700

    create train

---
+++ RL-implementaions/lunarlander_a2c/train.py
@@ -0,0 +1,71 @@
+import gymnasium as gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import random
+from collections import deque
+from memory import Memory
+from model import ActorNetwork, CriticNetwork
+
+env = gym.make("LunarLander-v3")
+
+# hyperparameters
+
+gamma = 0.99
+learning_rate = 0.001
+num_episodes = 1000
+actor = ActorNetwork(env)
+actor_optimizer = optim.Adam(actor.parameters(), lr = learning_rate)
+critic = CriticNetwork(env)
+critic_optimizer = optim.Adam(critic.parameters(), lr = learning_rate)
+memory = Memory()
+maxsteps = 200
+
+# train func
+def train(memory, q_val):
+    values = torch.stack(memory.values)
+    q_vals = np.zeros(len(memory), 1)
+    # target values are calculated backward
+    for i, (_, _, reward, done) in enumerate(memory.reversed()):
+        q_val = reward + (1-done)*gamma*q_val
+        q_vals[len(memory)-1-i] = q_val # store values from the end to the beginning
+    advantage = torch.Tensor(q_vals) - values
+    #update actor
+    actor_loss = (-torch.stack(memory.log_probs)*advantage.detach()).mean()
+    actor_optimizer.zero_grad()
+    actor_loss.backward()
+    actor_optimizer.step()
+    #update critic
+    critic_loss = advantage.pow(2).mean()
+    critic_optimizer.zero_grad()
+    critic_loss.backward()
+    critic_optimizer.step()
+    
+episode_reward = []
+for episode in range(num_episodes):
+    state = env.reset()
+    done = False
+    total_reward = 0
+    steps = 0
+    while not done:
+        action_probs = actor(state)
+        dist = torch.distributions.Categorical(probs = action_probs)
+        action = dist.sample()
+        log_prob = dist.log_prob(action)
+        next_state, reward, done, info = env.step(action.item())
+        state = next_state
+        total_reward += reward
+        steps += 1
+        memory.add(log_prob, critic(state), reward, done)
+        if done or steps >= maxsteps:
+            break
+        train(memory, total_reward)
+        episode_reward.append(total_reward)
+        print(f"Episode {episode + 1}/{num_episodes}, Total Reward: {total_reward}")
+
+torch.save(actor.state_dict(), 'actor_model.pth')
+torch.save(critic.state_dict(), 'critic_model.pth')
+        
+    
+    
